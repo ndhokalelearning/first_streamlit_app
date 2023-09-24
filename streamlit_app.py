03a2a747fa354fbb954704b9c35963a5a9c85d52
ndhokalelearning <72453303+ndhokalelearning@users.noreply.github.com>
2023-09-23 21:18:06 -0500

    Update streamlit_app.py
    added snowflake functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-# cerate function
+# create function
 def get_fruityvice_data (this_fruit_choice) :
   fruityvice_response = fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -45,23 +45,23 @@ try:
 except URLError as e:
   steramlit.error()
   
+
+streamlit.header("Fruit load list contains:")
+# snowfalek related functions
+def get_fruit_load_list():
+  with cnx_cursor as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a button to load a fruit
+if streamlit.button('get fruit load list') :
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
   
+
 #don't run any more code !!!
 streamlit.stop()
-
-#import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text("Fruit load list contains:")
-#streamlit.text(my_data_row)
-streamlit.header("Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruit_selected_2 = streamlit.multiselect("Pick a fruit:", list(my_fruit_list.index))
